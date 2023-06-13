9caeb308863b537b2570f7ec8607fac6f2e10430
Denis Vasilev <pythocoder@gmail.com>
2023-06-14 04:59:28 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,55 @@
+import os
+import time
+
+
+def logger(old_function):
+
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+        with open('main.log', 'a+') as log_file:
+            date_and_time = time.gmtime()
+            log_file.write(f'{date_and_time[5]}:{date_and_time[4]}, {date_and_time[3]}/{date_and_time[1]}/{date_and_time[0]}, {old_function.__name__}, {args}, {kwargs}, {result}\n')
+        return result
+
+    return new_function
+
+
+def test_1():
+    path = 'main.log'
+    if os.path.exists(path):
+        os.remove(path)
+
+    @logger
+    def hello_world():
+        return 'Hello World'
+
+    @logger
+    def summator(a, b=0):
+        return a + b
+
+    @logger
+    def div(a, b):
+        return a / b
+
+    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+    result = summator(2, 2)
+    assert isinstance(result, int), 'Должно вернуться целое число'
+    assert result == 4, '2 + 2 = 4'
+    result = div(6, 2)
+    assert result == 3, '6 / 2 = 3'
+
+    assert os.path.exists(path), 'файл main.log должен существовать'
+
+    summator(4.3, b=2.2)
+    summator(a=0, b=0)
+
+    with open(path) as log_file:
+        log_file_content = log_file.read()
+
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    for item in (4.3, 2.2, 6.5):
+        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_1()
